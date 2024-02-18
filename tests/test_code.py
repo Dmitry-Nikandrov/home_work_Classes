cf3c23eed3b8518190934b7e632f295c7cfe26f3
Nikandrov Dmitry <dnikandrov@yandex.ru>
2024-02-18 12:30:12 +0300

    Classes_finish_18/02/2024

--- tests/test_code.py
+++ tests/test_code.py
@@ -34,15 +34,31 @@ def test_cat(Category_phone):
 
 
 
+
+@pytest.fixture
+def test_list():
+    return [{'name': 'Смартфоны', 'description': 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни', 'products': [{'name': 'Samsung Galaxy C23 Ultra', 'description': '256GB, Серый цвет, 200MP камера', 'price': 180000.0, 'quantity': 5}, {'name': 'Iphone 15', 'description': '512GB, Gray space', 'price': 210000.0, 'quantity': 8}, {'name': 'Xiaomi Redmi Note 11', 'description': '1024GB, Синий', 'price': 31000.0, 'quantity': 14}]}, {'name': 'Телевизоры', 'description': 'Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником', 'products': [{'name': '55 QLED 4K', 'description': 'Фоновая подсветка', 'price': 123000.0, 'quantity': 7}]}]
+
+
 def test_load_json():
     assert load_json('operations_test.json') == [{"name": "Смартфоны","description": "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни","products": [{"name": "Samsung Galaxy C23 Ultra","description": "256GB, Серый цвет, 200MP камера","price": 180000.0,"quantity": 5}]}]
 
-def test_create_Prod():
-    assert create_Prod([{"name": "Смартфоны","description": "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни","products": [{"name": "Samsung Galaxy C23 Ultra","description": "256GB, Серый цвет, 200MP камера","price": 180000.0,"quantity": 5}]}]) == None
-def test_count_Prod():
-    assert count_Prod([{'name': 'Смартфоны', 'description': 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни', 'products': [{'name': 'Samsung Galaxy C23 Ultra', 'description': '256GB, Серый цвет, 200MP камера', 'price': 180000.0, 'quantity': 5}, {'name': 'Iphone 15', 'description': '512GB, Gray space', 'price': 210000.0, 'quantity': 8}, {'name': 'Xiaomi Redmi Note 11', 'description': '1024GB, Синий', 'price': 31000.0, 'quantity': 14}]}, {'name': 'Телевизоры', 'description': 'Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником', 'products': [{'name': '55 QLED 4K', 'description': 'Фоновая подсветка', 'price': 123000.0, 'quantity': 7}]}]) == 'Количество уникальных продуктов - 4'
+def test_count_Prod(test_list):
+    assert count_Prod(test_list) == 4
 
-def test_create_Cat():
-    assert create_Cat([{'name': 'Смартфоны', 'description': 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни', 'products': [{'name': 'Samsung Galaxy C23 Ultra', 'description': '256GB, Серый цвет, 200MP камера', 'price': 180000.0, 'quantity': 5}, {'name': 'Iphone 15', 'description': '512GB, Gray space', 'price': 210000.0, 'quantity': 8}, {'name': 'Xiaomi Redmi Note 11', 'description': '1024GB, Синий', 'price': 31000.0, 'quantity': 14}]}, {'name': 'Телевизоры', 'description': 'Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником', 'products': [{'name': '55 QLED 4K', 'description': 'Фоновая подсветка', 'price': 123000.0, 'quantity': 7}]}])
+def test_create_Prod(test_list):
+    assert create_Prod(test_list)[0].name == 'Samsung Galaxy C23 Ultra'
+    assert create_Prod(test_list)[1].name == 'Iphone 15'
+    assert create_Prod(test_list)[2].description == '1024GB, Синий'
 
+
+
+def test_create_Cat(test_list):
+    assert create_Cat(test_list)[0].name == 'Смартфоны'
+    assert create_Cat(test_list)[0].description == 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни'
+    assert create_Cat(test_list)[1].product == '55 QLED 4K'
+    assert create_Cat(test_list)[1].name == 'Телевизоры'
+
+def test_count_Cat(test_list):
+    assert count_Cat(test_list) == 2
 
